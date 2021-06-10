5d7db13b196fc61bed66c79073369018096dc05f
lql <liqiuli@outlook.com>
2021-06-10 11:35:07 +1000

    Added Network ApiService   - Implemented the network access with Retrofit   - Implemented a API to fetch user info from backend endpoint with coroutine   - Displaying the real data from backend API instead of local data

--- app/src/main/java/com/example/sampletest/ui/user/UserViewModel.kt
+++ app/src/main/java/com/example/sampletest/ui/user/UserViewModel.kt
@@ -3,11 +3,14 @@ package com.example.sampletest.ui.user
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.sampletest.data.User
 import com.example.sampletest.data.sampleData
+import com.example.sampletest.network.Api
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.launch
 
 class UserViewModel : ViewModel() {
 
@@ -19,7 +22,8 @@ class UserViewModel : ViewModel() {
         get() = _userData
 
     init {
-        getDataFromSample()
+//        getDataFromSample()
+        getDataFromNetworkCoroutine()
     }
 
     private fun getDataFromSample() {
@@ -30,5 +34,15 @@ class UserViewModel : ViewModel() {
         val jsonAdapter = moshi.adapter<List<User>>(listMyData)
         val sampleData = jsonAdapter.fromJson(sampleData)
         _userData.value = sampleData ?: emptyList<User>()
+    }
+
+    private fun getDataFromNetworkCoroutine() {
+        viewModelScope.launch {
+            try {
+                _userData.value = Api.retrofitService.getDataCoroutine()
+            } catch (e: Exception) {
+                _userData.value = emptyList()
+            }
+        }
     }
 }
